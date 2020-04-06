043b82f60d25d556af979215b91717506ad55623
Bryan Siegel <bryansiegel@gmail.com>
2020-04-06 15:41:29 -0700

    minor code reformat

--- calendars.py
+++ calendars.py
@@ -20,17 +20,17 @@ import calendar
 
 # iterate over day names
 # for day in calendar.day_name:
-    # print(day)
+# print(day)
 
 print('team meetings will be on')
-for m in range(1,13):
+for m in range(1, 13):
     cal = calendar.monthcalendar(2020, m)
     weekone = cal[0]
-    weektwo =  cal[1]
+    weektwo = cal[1]
 
     if weekone[calendar.FRIDAY != 0]:
         meetday = weekone[calendar.FRIDAY]
     else:
         meetday = weektwo[calendar.FRIDAY]
-        
+
     print("%10s %2d" % (calendar.month_name[m], meetday))
--- dates.py
+++ dates.py
@@ -2,21 +2,20 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-
 # today = date.today()
 
 # print(today)
 
 # print(today.day, today.month, today.year)
 
-#get today's number
+# get today's number
 # print("todays weekday number is ", today.weekday())
 
-#get todays date
+# get todays date
 # days =['mon', 'tues', 'weds', 'thurs', 'fri', 'sat']
 # print('Which is a: ', days[today.weekday()])
 
-#get date and time
+# get date and time
 # today = datetime.now()
 # print(today)
 
