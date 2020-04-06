f7e2c9699fcf4b65d0fdbd260a97569efa472980
Bryan Siegel <bryansiegel@gmail.com>
2020-04-06 11:05:22 -0700

    datetime

---
+++ dates.py
@@ -0,0 +1,25 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+
+# today = date.today()
+
+# print(today)
+
+# print(today.day, today.month, today.year)
+
+#get today's number
+# print("todays weekday number is ", today.weekday())
+
+#get todays date
+# days =['mon', 'tues', 'weds', 'thurs', 'fri', 'sat']
+# print('Which is a: ', days[today.weekday()])
+
+#get date and time
+# today = datetime.now()
+# print(today)
+
+# get just time
+# t = datetime.time(datetime.now())
+# print(t)
