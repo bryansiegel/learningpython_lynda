f3dcbd56fae2556bdc1bbd52d7dad76a6554e011
Bryan Siegel <bryansiegel@gmail.com>
2020-04-06 10:43:41 -0700

    Classes

---
+++ classes.py
@@ -0,0 +1,23 @@
+class myClass():
+    def method1(self):
+        print('myclass method1')
+
+    def method2(self, someString):
+        print('myclass method2', someString)
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print('anotherClass method1')
+
+    def method2(self):
+        print('anotherClass method2')
+
+c = myClass()
+c.method1()
+c.method2('somestring')
+
+c2 = anotherClass()
+c2.method1()
+c2.method2()
