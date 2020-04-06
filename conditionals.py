1e25af9e45f681c9fb79049a6627daf8a52c9889
Bryan Siegel <bryansiegel@gmail.com>
2020-04-06 10:03:34 -0700

    loops

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+def main():
+    x,y = 10, 100
+
+    if(x < y):
+        st = "x is less than y"
+
+    print(st)
+
+if __name__ == "__main__":
+    main()
---
+++ loops.py
@@ -0,0 +1,18 @@
+x = 0
+
+# while(x<5):
+#     print(x)
+#     x = x + 1
+
+# for x in range(5,10):
+#     print(x)
+
+days = ['mon', 'tues', 'weds']
+
+for i,d in enumerate(days):
+    print(i, d)
+
+# for x in range(5,10):
+#     # if(x == 7):break
+#     if(x % 2 == 0) :continue
+#     print(x)
